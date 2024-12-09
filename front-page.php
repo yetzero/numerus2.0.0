b7c012c292cab76f4dffbfd813cb0a76dc110062
Luis Santander <yetzero@gmail.com>
2024-12-09 12:31:17 -0300

    Revert "Remove commented-out hero section code from front-page.php"
    This reverts commit 40d8c6eef0e8928ca3ac18726851b9305a83fc56.

--- front-page.php
+++ front-page.php
@@ -15,8 +15,30 @@
 get_header();
 ?>
 
+<!-- ======= Hero Section ======= -->
 <!-- Start the Loop. -->
  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+ <?php $page_slug = get_post_field('post_name', get_post()); $thumb_id = get_post_thumbnail_id(); $thumb_url = wp_get_attachment_image_src($thumb_id,'full', true); ?>
+<section id="hero" <?php post_class('hero d-flex align-items-end'); ?> style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+	<article class="wrapp-hero wx-100 mb-5">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="inner-hero"> 
+					<!-- <p class="title-hero" class="text-light">Colegios más</p> -->
+					<h1 class="titleheading">
+						<span class="paragraph" class="text-light">Colegios más</span>
+						<span id="element" class="element"></span></h1> 
+					</div>
+				</div>
+			</div>
+			</div>
+			<!-- <div class="wrapp-btn-hero"> 
+				<a href="#about" class="btn-hero scrollto "><i class="bi bi-arrow-down-circle"></i></a>
+			</div> -->
+		</div>
+	</article>
+</section><!-- End Hero -->
 <?php the_content(); ?>
 
 <?php endwhile; else : ?>
